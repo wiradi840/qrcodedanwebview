d336e6b59b60dbc54180a9e0ace605b65894f654
wiraadi840 <wira@undiksha.ac.id>
2020-06-18 09:47:10 +0800

    Belajar QR Code dan Webview

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+
+import './webview_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String kode = "";
+  var getKode;
+  Future scanBarcode() async {
+    getKode = await BarcodeScanner.scan();
+    setState(() {
+      kode = getKode.rawContent;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FLUTTER QR CODE AND WEBVIEW"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 10,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16.0),
+                bottomRight: Radius.circular(16.0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Image.network(
+              "https://cdn.searchenginejournal.com/wp-content/uploads/2014/04/shutterstock_115485979.jpg"),
+          SizedBox(
+            height: 10.0,
+          ),
+          FlatButton(
+            onPressed: () {
+              scanBarcode();
+            },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    "SCAN QR CODE",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+          FlatButton(
+            onPressed: kode == ""
+                ? () {}
+                : () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WebScreen(kode),
+                      ),
+                    );
+                  },
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: kode == ""
+                      ? Text(
+                          "HASIL",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        )
+                      : Text(
+                          kode,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                          ),
+                        ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
